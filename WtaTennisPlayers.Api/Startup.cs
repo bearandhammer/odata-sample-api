d5b1ecaed484240d112e1de55ffd5c5e79af656c
Lewis Grint <51135182+bearandhammer@users.noreply.github.com>
2020-06-09 21:37:27 +0100

    Updating to .NET Core 3.1.
    & Options.EnableEndpointRouting = false in order to tweak the route builder (fix for the 'Endpoint Routing does not support 'IApplicationBuilder.UseMvc(...)'. To use 'IApplicationBuilder.UseMvc' set 'MvcOptions.EnableEndpointRouting = false' inside 'ConfigureServices(...).' exception).

--- WtaTennisPlayers.Api/Startup.cs
+++ WtaTennisPlayers.Api/Startup.cs
@@ -19,7 +19,7 @@ namespace WtaTennisPlayers.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
 
             // Add OData and Memory Cache services
             services.AddOData();
